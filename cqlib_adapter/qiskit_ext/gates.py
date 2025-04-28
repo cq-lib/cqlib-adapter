f7a3f1c0e3662b26e4b03431f02c998b5c35e55c
Gao Jianjian <jianjian001@outlook.com>
2025-04-28 15:32:18 +0800

    update(gates.py): update Gate define and equivalence

--- cqlib_adapter/qiskit_ext/gates.py
+++ cqlib_adapter/qiskit_ext/gates.py
@@ -15,8 +15,8 @@ from math import pi
 import numpy as np
 from qiskit.quantum_info import Operator
 from qiskit.circuit import Gate, QuantumCircuit, Parameter
-from qiskit.circuit.library import UnitaryGate, GlobalPhaseGate, RXGate, RYGate, \
-    HGate, XGate, YGate, CXGate
+from qiskit.circuit.library import GlobalPhaseGate, RXGate, RYGate, \
+    HGate, XGate, YGate
 from qiskit.circuit.equivalence_library import SessionEquivalenceLibrary as SELib
 
 from cqlib.circuits.gates.x import X2P, X2M
@@ -24,17 +24,15 @@ from cqlib.circuits.gates.y import Y2P, Y2M
 from cqlib.circuits.gates.xy import XY2P, XY2M
 
 
-# gate define
-# x2p_gate = UnitaryGate(np.asarray(X2P()), label="x2p", num_qubits=1)
-# x2m_gate = UnitaryGate(np.asarray(X2M()), label="x2m", num_qubits=1)
-# y2p_gate = UnitaryGate(np.asarray(Y2P()), label="y2p", num_qubits=1)
-# y2m_gate = UnitaryGate(np.asarray(Y2M()), label='y2m', num_qubits=1)
-
-
 # X2P
 class X2PGate(Gate):
     def __init__(self, label=None):
         super().__init__("x2p", 1, params=[], label=label)
+
+    def _define(self):
+        defn = QuantumCircuit(1)
+        defn.rx(pi / 2, 0)
+        self._definition = defn
 
     def __array__(self, dtype=None, copy=None):
         if copy is False:
@@ -47,6 +45,11 @@ class X2MGate(Gate):
     def __init__(self, label=None):
         super().__init__("x2m", 1, [], label=label)
 
+    def _define(self):
+        defn = QuantumCircuit(1)
+        defn.rx(-pi / 2, 0)
+        self._definition = defn
+
     def __array__(self, dtype=None, copy=None):
         if copy is False:
             raise ValueError("unable to avoid copy while creating an array as requested")
@@ -58,6 +61,11 @@ class Y2PGate(Gate):
     def __init__(self, label=None):
         super().__init__("y2p", 1, params=[], label=label)
 
+    def _define(self):
+        defn = QuantumCircuit(1)
+        defn.ry(pi / 2, 0)
+        self._definition = defn
+
     def __array__(self, dtype=None, copy=None):
         if copy is False:
             raise ValueError("unable to avoid copy while creating an array as requested")
@@ -68,6 +76,11 @@ class Y2PGate(Gate):
 class Y2MGate(Gate):
     def __init__(self, label=None):
         super().__init__("y2m", 1, [], label=label)
+
+    def _define(self):
+        defn = QuantumCircuit(1)
+        defn.ry(-pi / 2, 0)
+        self._definition = defn
 
     def __array__(self, dtype=None, copy=None):
         if copy is False:
@@ -94,23 +107,17 @@ class XY2PGate(Gate):
         return np.asarray(XY2P(self.params[0]), dtype=dtype)
 
 
-x2p_gate = X2PGate()
-x2m_gate = X2MGate()
-y2p_gate = Y2PGate()
-y2m_gate = Y2MGate()
-
-
 # XY2M
 class XY2MGate(Gate):
     def __init__(self, theta, label=None):
         super().__init__("xy2m", 1, [theta], label=label)
 
     def _define(self):
-        theta = self.params[0]
+        theta_ = self.params[0]
         defn = QuantumCircuit(1)
-        defn.rz(-pi / 2 - theta, 0)
+        defn.rz(-pi / 2 - theta_, 0)
         defn.ry(pi / 2, 0)
-        defn.rz(theta + pi / 2, 0)
+        defn.rz(theta_ + pi / 2, 0)
         self._definition = defn
 
     def __array__(self, dtype=None, copy=None):
@@ -122,19 +129,19 @@ class XY2MGate(Gate):
 # equivalence rules
 # rx pi/2
 rx_qc = QuantumCircuit(1)
-rx_qc.append(x2p_gate, [0])
-SELib.add_equivalence(RXGate(pi / 2), rx_qc)
+rx_qc.append(X2PGate(), [0])
+# SELib.add_equivalence(RXGate(pi / 2), rx_qc)
 assert np.allclose(np.asarray(RXGate(pi / 2)), Operator(rx_qc).to_matrix())
 
 x_qc = QuantumCircuit(1)
 x_qc.rx(pi / 2, 0)
-SELib.add_equivalence(x2p_gate, x_qc)
-assert np.allclose(np.asarray(x2p_gate), Operator(x_qc).to_matrix())
+SELib.add_equivalence(X2PGate(), x_qc)
+assert np.allclose(np.asarray(X2PGate()), Operator(x_qc).to_matrix())
 
 # x
 x_qc = QuantumCircuit(1)
-x_qc.append(x2p_gate, [0])
-x_qc.append(x2p_gate, [0])
+x_qc.append(X2PGate(), [0])
+x_qc.append(X2PGate(), [0])
 x_qc.append(GlobalPhaseGate(pi / 2), [])
 SELib.add_equivalence(XGate(), x_qc)
 assert np.allclose(np.asarray(XGate()), Operator(x_qc).to_matrix())
@@ -143,9 +150,9 @@ assert np.allclose(np.asarray(XGate()), Operator(x_qc).to_matrix())
 theta = Parameter("theta")
 rx_qc = QuantumCircuit(1)
 rx_qc.rz(pi / 2, 0)
-rx_qc.append(x2p_gate, [0])
+rx_qc.append(X2PGate(), [0])
 rx_qc.rz(theta, 0)
-rx_qc.append(x2m_gate, [0])
+rx_qc.append(X2MGate(), [0])
 rx_qc.rz(-pi / 2, 0)
 SELib.add_equivalence(RXGate(theta), rx_qc)
 
@@ -156,9 +163,9 @@ for i in range(5):
 # RY
 theta = Parameter("theta")
 ry_qc = QuantumCircuit(1)
-ry_qc.append(x2p_gate, [0])
+ry_qc.append(X2PGate(), [0])
 ry_qc.rz(theta, 0)
-ry_qc.append(x2m_gate, [0])
+ry_qc.append(X2MGate(), [0])
 SELib.add_equivalence(RYGate(theta), ry_qc)
 
 for i in range(5):
@@ -167,40 +174,40 @@ for i in range(5):
 
 # X2M
 x2m_qc = QuantumCircuit(1)
-x2m_qc.append(x2m_gate, [0])
-SELib.add_equivalence(RXGate(-pi / 2), x2m_qc)
+x2m_qc.append(X2MGate(), [0])
+# SELib.add_equivalence(RXGate(-pi / 2), x2m_qc)
 assert np.allclose(np.asarray(RXGate(-pi / 2)), Operator(x2m_qc).to_matrix())
 
 # X2M
 x2m_qc = QuantumCircuit(1)
 x2m_qc.rx(-pi / 2, 0)
-SELib.add_equivalence(x2m_gate, x2m_qc)
-assert np.allclose(np.asarray(x2m_gate), Operator(x2m_qc).to_matrix())
+SELib.add_equivalence(X2MGate(), x2m_qc)
+assert np.allclose(np.asarray(X2MGate()), Operator(x2m_qc).to_matrix())
 
 # Y2P
 y2p_qc = QuantumCircuit(1)
-y2p_qc.append(y2p_gate, [0])
-SELib.add_equivalence(RYGate(pi / 2), y2p_qc)
+y2p_qc.append(Y2PGate(), [0])
+# SELib.add_equivalence(RYGate(pi / 2), y2p_qc)
 assert np.allclose(np.asarray(RYGate(pi / 2)), Operator(y2p_qc).to_matrix())
 
 # Y
 y_qc = QuantumCircuit(1)
-y_qc.append(y2p_gate, [0])
-y_qc.append(y2p_gate, [0])
+y_qc.append(Y2PGate(), [0])
+y_qc.append(Y2PGate(), [0])
 y_qc.append(GlobalPhaseGate(pi / 2), [])
 SELib.add_equivalence(YGate(), y_qc)
 assert np.allclose(np.asarray(YGate()), Operator(y_qc).to_matrix())
 
 # X2M
 y2m_qc = QuantumCircuit(1)
-y2m_qc.append(y2m_gate, [0])
-SELib.add_equivalence(RYGate(-pi / 2), y2m_qc)
+y2m_qc.append(Y2MGate(), [0])
+# SELib.add_equivalence(RYGate(-pi / 2), y2m_qc)
 assert np.allclose(np.asarray(RYGate(-pi / 2)), Operator(y2m_qc).to_matrix())
 
 #  H gate
 h_decomp = QuantumCircuit(1)
 h_decomp.rz(pi, 0)
-h_decomp.append(y2p_gate, [0])
+h_decomp.append(Y2PGate(), [0])
 h_decomp.append(GlobalPhaseGate(pi / 2), [])
 SELib.add_equivalence(HGate(), h_decomp)
 assert np.allclose(np.asarray(HGate()), Operator(h_decomp).to_matrix())
@@ -236,10 +243,10 @@ for i in range(5):
     assert Operator(xy2m_qc.assign_parameters([t])).equiv(Operator(qc.assign_parameters([t])))
 
 __all__ = [
-    'x2p_gate',
-    'x2m_gate',
-    'y2p_gate',
-    'y2m_gate',
+    'X2PGate',
+    'X2MGate',
+    'Y2PGate',
+    'Y2MGate',
     'XY2PGate',
     'XY2MGate'
 ]
