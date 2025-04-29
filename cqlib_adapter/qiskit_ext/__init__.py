70c9e250e7a07e020eae25623161c5cc107f439b
Gao Jianjian <jianjian001@outlook.com>
2025-04-29 11:03:33 +0800

    feat(cqlib_adapter): add new components and update existing ones
    - Add adapter.py, api_client.py, job.py, and sampler.py modules
    - Implement new classes: ApiClient, TianYanJob, and TianYanSampler
    - Include additional gate classes: X2PGate, X2MGate, Y2PGate, Y2MGate, XY2PGate, XY2MGate
    - Update __init__.py to import new components
    - Add copyright notice and licensing information to new files

--- cqlib_adapter/qiskit_ext/__init__.py
+++ cqlib_adapter/qiskit_ext/__init__.py
@@ -11,10 +11,27 @@
 # that they have been altered from the originals.
 
 
+from .adapter import to_cqlib
+from .api_client import ApiClient
+from .gates import X2PGate, X2MGate, Y2PGate, Y2MGate, XY2MGate, XY2PGate
+from .job import TianYanJob
+from .sampler import TianYanSampler
 from .tianyan_provider import TianYanProvider
 from .tianyan_backend import TianYanBackend
 
 __all__ = [
+    "to_cqlib",
+    "ApiClient",
+
+    'X2PGate',
+    'X2MGate',
+    'Y2PGate',
+    'Y2MGate',
+    'XY2PGate',
+    'XY2MGate',
+
+    "TianYanJob",
+    "TianYanSampler",
     "TianYanProvider",
     "TianYanBackend",
 ]
