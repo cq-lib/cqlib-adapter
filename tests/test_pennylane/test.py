1ca5a40f3d9397c1855a42a414c729633160006c
阚寅 <13017899+hefei-wu-yanzu@user.noreply.gitee.com>
2025-08-22 08:33:50 +0000

    update tests/test_pennylane/test.py.
    Signed-off-by: 阚寅 <13017899+hefei-wu-yanzu@user.noreply.gitee.com>

--- tests/test_pennylane/test.py
+++ tests/test_pennylane/test.py
@@ -5,8 +5,7 @@ from pennylane import numpy as np
 TOKEN = "your_token"
 # 如果只使用本地模拟器，可以将login_key设置为None
 dev = qml.device('cqlib.device', wires=2, shots=500, cqlib_backend_name="default",login_key = TOKEN)
-
-
+# 构建一个测试线路
 @qml.qnode(dev, diff_method="parameter-shift")
 def circuit(params):
     qml.RX(params[0], wires=0)
@@ -14,6 +13,15 @@ def circuit(params):
     qml.CNOT(wires=[0, 1])
     return qml.expval(qml.PauliY(0))
 params = np.array([0.5, 0.8], requires_grad=True)
+
+# 进行简单的优化（模拟量子机器学习）
+opt = qml.GradientDescentOptimizer(stepsize=0.1)
+steps = 10
+for i in range(steps):
+    params = opt.step(circuit, params)
+
+    print(f"步骤 {i + 1}: 参数 = {params}, 期望值 = {circuit(params)}")
+
 # 初始化设备
 def test(cqlib_backend_name):
     dev = qml.device('cqlib.device', wires=2, shots=500, cqlib_backend_name=cqlib_backend_name)
@@ -23,7 +31,7 @@ def test(cqlib_backend_name):
         qml.RX(params[0], wires=0)
         qml.RY(params[1], wires=1)
         qml.CNOT(wires=[0, 1])
-        return qml.probs(wires=[0, 1])  # 返回4个概率值
+        return qml.probs(wires=[0, 1])  # 返回概率值
 
     @qml.qnode(dev)
     def circuit_expval(params):
@@ -38,16 +46,8 @@ def test(cqlib_backend_name):
     print("概率分布:", probs)
     print("期望值:", expval)
 
+
+# 后端测试
 # test("tianyan504")
 # test("tianyan_sw")
 # test("default")
-# 进行简单的优化（模拟量子机器学习）
-opt = qml.GradientDescentOptimizer(stepsize=0.1)
-steps = 10
-for i in range(steps):
-    params = opt.step(circuit, params)
-
-    print(f"步骤 {i + 1}: 参数 = {params}, 期望值 = {circuit(params)}")
-
-# print("最终参数:", params)
-# print("最终期望值:", circuit(params))
